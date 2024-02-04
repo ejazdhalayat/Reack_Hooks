3f48cab74478290073c6921ff3950f53acc35bdc
Ejaz <ejazdhalayat45@gmail.com>
2024-02-04 21:48:10 +0530

    Colorswitch completed

--- src/Components/usestate/ColorSwitch.jsx
+++ src/Components/usestate/ColorSwitch.jsx
@@ -1,18 +1,23 @@
-import React from 'react'
+import React, { useState } from "react";
 
 export const ColorSwitch = () => {
+  const [bgColor, setBgColor] = useState("");
+  const color = (e) => setBgColor(e.target.value);
   return (
-    <div className='max-w-sm rounded  shadow-lg border h-auto px-4 py-1 '>
-        <select className='bg-transparent rounded-md border border-black '>
-            <option value="">Choose color</option>
-            <option valuie="red">Red</option>
-            <option valuie="blue">Blue</option>
-            <option valuie="green">Green</option>
-            <option valuie="yellow">Yellow</option>
-        </select>
-        <div className=''>
-
-        </div>
+    <div className="max-w-sm rounded  shadow-lg border h-auto px-4 py-1 ">
+      <select
+        className="bg-transparent rounded-md border border-black"
+        onChange={color}
+      >
+        <option value="">Choose color</option>
+        <option value="red">Red</option>
+        <option value="blue">Blue</option>
+        <option value="green">Green</option>
+        <option value="yellow">Yellow</option>
+      </select>
+      <div
+        style={{ backgroundColor: bgColor, height: "100px", width: "100px" }}
+      ></div>
     </div>
-  )
-}
+  );
+};
