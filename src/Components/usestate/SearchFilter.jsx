5088570aa7794738be3248cc8b7b0c87b15ce1e4
Ejaz <ejazdhalayat45@gmail.com>
2024-02-04 21:57:24 +0530

    Search Filter

---
+++ src/Components/usestate/SearchFilter.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+
+export const SearchFilter = () => {
+    const [filter, setfilter] = useState([]);
+    const items = ['Apple', 'Banana', 'Carrot', 'Date', 'Eggplant', 'Fig', 'Grape'];
+
+  return (
+    <div>
+        <input />
+
+    </div>
+  )
+}
